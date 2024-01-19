4083e8ad895cfafc56c6b437da4690bb2525521b
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-01-19 02:40:25 +0100

    add functionnality

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -9,5 +9,32 @@ module.exports={
             console.error('Erreur lors de la récupération des utilisateurs dans le contrôleur :', error);
             return res.status(500).json({ message: 'Erreur serveur' });
         }
+    },
+    getUserById:async (req,res)=>{
+        const userId=req.params.id;
+        try{
+            const user=await User.findById(userId);
+            if (!user) {
+                return res.status(404).json({ message: 'Utilisateur non trouvé' });
+            }
+            return res.status(200).json(user);
+        }catch (error){
+            console.error('Erreur lors de la récupération de l\'utilisateur dans le contrôleur :', error);
+            return res.status(500).json({ message: 'Erreur serveur' });
+        }
+    },
+    addUsers:async (req,res)=>{
+        const {username,email,password}=req.body
+        try {
+            const newUser = new User(req.body); // Utilisez req.body pour récupérer les données du corps de la requête
+    
+            // Enregistrez le nouvel utilisateur dans la base de données
+            const savedUser = await newUser.save();
+    
+            return res.status(201).json(savedUser);
+        } catch (error) {
+            console.error('Erreur lors de l\'ajout de l\'utilisateur :', error);
+            return res.status(500).json({ message: 'Erreur serveur' });
+        }
     }
 }
--- app/routes/userRoute.js
+++ app/routes/userRoute.js
@@ -2,8 +2,8 @@ const router=require('express').Router();
 const userController=require('../controllers/userController')
 
 router.get('/', userController.getUsers);
-// router.get('/:id', userController.getUserById);
-// router.post('/', userController.addUsers);
+router.get('/:id', userController.getUserById);
+router.post('/', userController.addUsers);
 // router.put('/:id', userController.updateUsers);
 // router.delete('/:id', userController.deleteUser);
 
