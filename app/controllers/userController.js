a268b3e86d6311795f2e3ad25134e767c84700af
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-01-20 04:04:46 +0100

    updateuser

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -7,7 +7,7 @@ module.exports={
             return res.status(200).json(users);
         } catch (error) {
             console.error('Erreur lors de la récupération des utilisateurs dans le contrôleur :', error);
-            return res.status(500).json({ message: 'Erreur serveur' });
+            return res.status(500).json({ error: 'Erreur serveur' });
         }
     },
     getUserById:async (req,res)=>{
@@ -20,7 +20,7 @@ module.exports={
             return res.status(200).json(user);
         }catch (error){
             console.error('Erreur lors de la récupération de l\'utilisateur dans le contrôleur :', error);
-            return res.status(500).json({ message: 'Erreur serveur' });
+            return res.status(500).json({ error: 'Erreur serveur' });
         }
     },
     addUsers:async (req,res)=>{
@@ -36,6 +36,42 @@ module.exports={
             return res.status(201).json(savedUser);
         } catch (error) {
             console.error('Erreur lors de l\'ajout de l\'utilisateur :', error);
+            return res.status(500).json({ error: 'Erreur serveur' });
+        }
+    },
+    deleteUser:async (req,res)=>{
+        const id=req.params.id;
+        try{
+            const user=await User.findByIdAndDelete(id);
+            if(!user){
+                res.status(404).json({message:"utilisateur not found"});
+            }
+            res.status(204).send();
+        }catch(e){
+            res.status(500).json({error:"'Erreur serveur'"})
+        }
+    },
+    updateUsers:async (req,res)=>{
+        const id=req.params.id;
+        const {username,email,password,preferences}=req.body;
+        try {
+            const existingUser=await User.find({$or:[{username},{email}]});
+            if (existingUser && existingUser._id.toString() !== id) {
+                return res.status(400).json({ message: 'L\'username ou l\'email est déjà utilisé par un autre utilisateur.' });
+            }
+            
+            const updatedUser = await User.findByIdAndUpdate(
+                id,
+                { username, email, password, preferences },
+                { new: true } // Pour obtenir l'utilisateur mis à jour dans la réponse
+            );
+            
+            if (!updatedUser) {
+                return res.status(404).json({ message: 'Utilisateur non trouvé.' });
+            }
+            return res.status(200).json(updatedUser);
+        } catch (error) {
+            console.error('Erreur lors de la mise à jour de l\'utilisateur :', error);
             return res.status(500).json({ message: 'Erreur serveur' });
         }
     }
--- app/routes/userRoute.js
+++ app/routes/userRoute.js
@@ -4,8 +4,8 @@ const userController=require('../controllers/userController')
 router.get('/', userController.getUsers);
 router.get('/:id', userController.getUserById);
 router.post('/', userController.addUsers);
-// router.put('/:id', userController.updateUsers);
-// router.delete('/:id', userController.deleteUser);
+router.put('/:id', userController.updateUsers);
+router.delete('/:id', userController.deleteUser);
 
 
 module.exports = router;
