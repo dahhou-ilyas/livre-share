97583da94725abc5f4677e12f67d581cb84a8967
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-01-23 18:44:58 +0100

    add rejected condition

--- app/controllers/exchangeController.js
+++ app/controllers/exchangeController.js
@@ -64,7 +64,42 @@ module.exports={
                 await Promise.all([man_who_requested.save(),user_owner.save()])
                 res.status(200).json({message:"echange est bine valide",status})
             }
-            // il faut ajouter assui une action pour rejected
+
+            // il est nouvellement ajouté
+            if(status==="Reject"){
+                const filter_owner = {
+                    '_id': user_ownerId,
+                    'notifications.content.user': man_who_requested_the_bookId,
+                    'notifications.content.book': bookId
+                  };
+                const update_owner = {
+                  $set: {
+                    'notifications.$.status': status
+                  },
+                };
+                const filter_request={
+                    '_id': man_who_requested_the_bookId,
+                    'exchangeRequests.fromUser':user_ownerId,
+                    'exchangeRequests.book':bookId
+                }
+                const update_request = {
+                    $pull: {
+                        'exchangeRequests': {
+                          'fromUser': user_ownerId,
+                          'book': bookId
+                        }
+                    }
+                }
+
+                const [result1, result2]=await Promise([
+                    User.findOneAndUpdate(filter_owner, update_owner, { new: true }),
+                    User.findOneAndUpdate(filter_request, update_request, { new: true })
+                ])
+                if(!result1 || !result2){
+                    res.status(404).status({message:"users or book not found"});
+                }
+                res.status(200).json({message : "exchange Rejected !!!!"})
+            }
         } catch (error) {
             res.status(500).json({error:"erreur dans la base de donné"})
         }
@@ -73,7 +108,7 @@ module.exports={
         res.send("eeeeeee");
     },
 
-    //cette methode n'est pas encore tester
+    //cette methode n'est pas encore tester mais il est bayna
     showExchangeRequestswithauser:async (req,res)=>{
         const userId=req.params.userId;
         const {fromUserId}=req.body;
@@ -82,7 +117,7 @@ module.exports={
                 _id: userId,
                 'exchangeRequests.fromUser': fromUserId,
             }, 'exchangeRequests.$').exec();
-            if (!specificExchangeRequests || specificExchangeRequests.length === 0) {
+            if ( !specificExchangeRequests) {
                 res.status(404).json({message:"L'utilisateur n'a pas été trouvé ou n'a pas d'exchangeRequests correspondants."})
             }
             res.status(200).json(specificExchangeRequests)
