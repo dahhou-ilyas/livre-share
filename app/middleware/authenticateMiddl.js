cdb8987a5d7584a419dd1123bc503925f9e5f934
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-01-31 20:16:53 +0100

    middleware jwt

---
+++ app/middleware/authenticateMiddl.js
@@ -0,0 +1,16 @@
+const jwt=require('jsonwebtoken');
+
+function AuthenticateToken(req,res,next){
+    const authHeader=req.headers['authorization'];
+    const token=authHeader && authHeader.split(' ')[1];
+    if(token==null) return res.status(401).json({message:"Unauthorized"}); 
+    jwt.verify(token,process.env.TOKEN_SECRET,(err,user)=>{
+        console.log(err);
+
+        if (err) return res.sendStatus(403);
+
+        req.user=user;
+
+        next();
+    })
+}
